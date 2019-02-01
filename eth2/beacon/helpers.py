20a505838cb38ae928f9324d6c82ada0788ab716
Hsiao-Wei Wang <hwwang156@gmail.com>
2019-02-01 23:34:10 +0800

    Add cache for simulation

--- eth2/beacon/helpers.py
+++ eth2/beacon/helpers.py
@@ -77,6 +77,7 @@ def _get_block_root(
     return latest_block_roots[slot % latest_block_roots_length]
 
 
+@functools.lru_cache(128)
 def get_block_root(
         state: 'BeaconState',
         slot: Slot,
