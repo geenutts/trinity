cf1f76c18d4b0aa494765b9f7291327c3b88a334
Hsiao-Wei Wang <hwwang156@gmail.com>
2019-02-19 02:58:30 +0800

    Add a very poor benchmarking for full attestation

---
+++ eth2_sim/simulation/test_full_attestation.py
@@ -0,0 +1,88 @@
+import time
+import json
+
+import rlp
+
+from eth.db.atomic import AtomicDB
+
+from eth2.beacon.state_machines.forks.serenity.blocks import (
+    SerenityBeaconBlock,
+)
+from eth2.beacon.state_machines.forks.serenity.configs import SERENITY_CONFIG
+from eth2.beacon.types.states import BeaconState
+from eth2_sim.simulation.beacon_utils import (
+    get_chain,
+)
+from eth2.beacon.tools.builder.validator import (
+    get_next_epoch_committee_assignment,
+)
+
+
+def run():
+    config = SERENITY_CONFIG
+    config = config._replace(
+        EPOCH_LENGTH=4,
+        TARGET_COMMITTEE_SIZE=20,
+        SHARD_COUNT=2,
+        MIN_ATTESTATION_INCLUSION_DELAY=2,
+    )
+    states = ()
+    blocks = ()
+
+    json_data = open('demo_blocks.json').read()
+    encoded_blocks = json.loads(json_data)
+    for block in encoded_blocks:
+        blocks += (rlp.decode(bytes.fromhex(block), SerenityBeaconBlock),)
+
+    json_data = open('demo_states.json').read()
+    encoded_states = json.loads(json_data)
+    for state in encoded_states:
+        states += (rlp.decode(bytes.fromhex(state), BeaconState),)
+
+    assert blocks[0].slot == 0
+    assert blocks[0].parent_root == b'\x00' * 32
+
+    chain = get_chain(config, states[0], blocks[0], AtomicDB())
+
+    for i in range(1, 4):
+        chain.import_block(blocks[i])
+        print(f'block {blocks[i].slot}, attestations={blocks[i].body.attestations}')
+
+    assert len(states[10].validator_registry) == 100
+    assignment = get_next_epoch_committee_assignment(
+        state=states[3],
+        config=config,
+        validator_index=0,
+        registry_change=False,
+    )
+    print('len(assignment.committee)', len(assignment.committee))
+    print('assignment.committee', assignment.committee)
+
+    start = time.time()
+    print('----start time----')
+    for i in range(4, 8):
+        if (states[i].slot + 1) % config.EPOCH_LENGTH == 0:
+            start_epoch = time.time()
+        chain.import_block(blocks[i])
+        if (states[i].slot + 1) % config.EPOCH_LENGTH == 0:
+            end_epoch = time.time()
+            print(f'epoch processing time: {end_epoch - start_epoch}')
+        print(f'block {blocks[i].slot}, attestations={blocks[i].body.attestations}')
+
+    for i in range(8, 12):
+        if (states[i].slot + 1) % config.EPOCH_LENGTH == 0:
+            start_epoch = time.time()
+        chain.import_block(blocks[i])
+        if (states[i].slot + 1) % config.EPOCH_LENGTH == 0:
+            end_epoch = time.time()
+            print(f'epoch processing time: {end_epoch - start_epoch}')
+        print(f'block {blocks[i].slot}, attestations={blocks[i].body.attestations}')
+
+    end = time.time()
+    total_time = end - start
+
+    print(f'total time: {total_time}')
+
+
+if __name__ == "__main__":
+    run()
