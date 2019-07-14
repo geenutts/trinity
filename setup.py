1d6168deae5aa93f133487e663ff3a21640f537b
Hsiao-Wei Wang <hwwang156@gmail.com>
2019-07-14 19:25:18 +0800

    Bump py-ssz to 0.1.0a12

--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ deps = {
         "py-ecc==1.6.0",
         "rlp>=1.1.0,<2.0.0",
         PYEVM_DEPENDENCY,
-        "ssz==0.1.0a11",
+        "ssz==0.1.0a12",
     ],
     'libp2p': [
         "base58>=1.0.3",
