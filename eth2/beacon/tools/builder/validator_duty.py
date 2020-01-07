d713c9205d8cc03298a2f725d71b82782032c7bd
Hsiao-Wei Wang <hwwang156@gmail.com>
2020-01-08 00:13:16 +0800

    Refactor `validator_duty`

--- eth2/beacon/tools/builder/validator_duty.py
+++ eth2/beacon/tools/builder/validator_duty.py
@@ -1,5 +1,7 @@
-from typing import Any, Dict, Iterable
+from dataclasses import dataclass
+from typing import Any, Dict, Iterable, Optional
 
+from eth_typing import BLSPubkey
 from eth_utils import ValidationError, encode_hex, to_tuple
 
 from eth2.beacon.committee_helpers import (
@@ -8,8 +10,24 @@ from eth2.beacon.committee_helpers import (
 )
 from eth2.beacon.state_machines.base import BaseBeaconStateMachine
 from eth2.beacon.types.states import BeaconState
-from eth2.beacon.typing import Epoch
+from eth2.beacon.typing import CommitteeIndex, Epoch, Slot, ValidatorIndex
 from eth2.configs import CommitteeConfig, Eth2Config
+
+
+@dataclass(frozen=True)
+class ValidatorDuty:
+    validator_pubkey: BLSPubkey
+    attestation_slot: Slot
+    committee_index: CommitteeIndex
+    block_proposal_slot: Slot
+
+    def to_encoded_dict(self) -> Dict[str, Any]:
+        return {
+            "validator_pubkey": encode_hex(self.validator_pubkey),
+            "attestation_slot": self.attestation_slot,
+            "committee_index": self.committee_index,
+            "block_proposal_slot": self.block_proposal_slot,
+        }
 
 
 @to_tuple
@@ -18,9 +36,13 @@ def get_validator_duties(
     state_machine: BaseBeaconStateMachine,
     epoch: Epoch,
     config: Eth2Config,
+    validator_index: Optional[ValidatorIndex] = None,
 ) -> Iterable[Dict[str, Any]]:
     current_epoch = state.current_epoch(config.SLOTS_PER_EPOCH)
     next_epoch = state.next_epoch(config.SLOTS_PER_EPOCH)
+
+    if validator_index is not None:
+        validator_pubkey = state.validators[validator_index].pubkey
 
     if epoch < current_epoch:
         raise ValidationError(
@@ -41,13 +63,34 @@ def get_validator_duties(
         state = state_machine.state_transition.apply_state_transition(
             state, future_slot=slot
         )
-        proposer_index = get_beacon_proposer_index(state, CommitteeConfig(config))
-        for validator_index in committee:
-            pubkey = state.validators[validator_index].pubkey
-            proposal_slot = slot if proposer_index == validator_index else None
-            yield {
-                "validator_pubkey": encode_hex(pubkey),
-                "attestation_slot": slot,
-                "committee_index": committee_index,
-                "block_proposal_slot": proposal_slot,
-            }
+
+        # Get duty of certain validator.
+        if validator_index in committee:
+            proposer_index = get_beacon_proposer_index(state, CommitteeConfig(config))
+            yield _get_validator_duty(
+                validator_index, slot, proposer_index, validator_pubkey, committee_index
+            )
+        else:
+            proposer_index = get_beacon_proposer_index(state, CommitteeConfig(config))
+            for validator_index in committee:
+                pubkey = state.validators[validator_index].pubkey
+                yield _get_validator_duty(
+                    validator_index, slot, proposer_index, pubkey, committee_index
+                )
+
+
+def _get_validator_duty(
+    validator_index: ValidatorIndex,
+    slot: Slot,
+    proposer_index: ValidatorIndex,
+    pubkey: BLSPubkey,
+    committee_index: CommitteeIndex,
+) -> Dict[str, Any]:
+    proposal_slot = slot if proposer_index == validator_index else None
+    duty = ValidatorDuty(
+        validator_pubkey=pubkey,
+        attestation_slot=slot,
+        committee_index=committee_index,
+        block_proposal_slot=proposal_slot,
+    )
+    return duty.to_encoded_dict()
